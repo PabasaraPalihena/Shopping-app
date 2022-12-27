5fd014f84f05212f7441f18fd1df9e88800f766f
PabasaraPalihena <pabapalihena@gmail.com>
2022-12-27 06:01:00 +0530

    search bar

--- client/src/components/Admin/Products.js
+++ client/src/components/Admin/Products.js
@@ -2,11 +2,53 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import Card from "../../common/MediaCard";
 import "./Viewproducts.css";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import { styled, alpha } from "@mui/material";
 
+//search bar
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.1),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.2),
+  },
+
+  width: "65%",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 const API = process.env.REACT_APP_API;
 
 export default function Products() {
   const [products, setproducts] = useState([]);
+  const [searchedValue, setSearchedValue] = useState("");
   useEffect(() => {
     Axios.get(`${API}api/v1/product/`)
       .then((res) => {
@@ -20,17 +62,39 @@ export default function Products() {
   return (
     <>
       <div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+              onChange={(e) => setSearchedValue(e.target.value)}
+            />
+          </Search>
+        </div>
         <div className="product_component">
-          {products.map((product) => {
-            return (
-              <Card
-                key={product._id.toString()}
-                product={product}
-                btn1="buy"
-                btn2="update"
-              />
-            );
-          })}
+          {products
+            .filter(
+              (row) =>
+                // note that I've incorporated the searchedVal length check here
+                !searchedValue.length ||
+                row.productName
+                  .toString()
+                  .toLowerCase()
+                  .includes(searchedValue.toString().toLowerCase())
+            )
+            .map((product) => {
+              return (
+                <Card
+                  key={product._id.toString()}
+                  product={product}
+                  btn1="buy"
+                  btn2="update"
+                />
+              );
+            })}
         </div>
       </div>
     </>
